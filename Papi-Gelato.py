e621d88b8cae6f8cad9c6644c676d3ee2b35760b
Reza <99068159@mydavinci.nl>
2021-10-29 13:52:35 +0200

    (4e) stap compleet

--- Papi-Gelato.py
+++ Papi-Gelato.py
@@ -3,14 +3,6 @@ bakjeLijst = ["bakje", "Bakje"]
 Yes = ["Yes", "yes", "Y", "y"]
 No = ["No", "no", "N", "n"]
 
-def Sorry():
-    print("Sorry dat sanp ik niet...")
-
-# def HoorntjeBakjeDEF():
-#     HoorntjeBakje = (input("Wilt u deze " + str(bolletjes) + " in een hoorntje of een bakje? :"))
-
-
-
 print("Welkom bij Papi Gelato je mag alle smaken kiezen zolang het maar vanille ijs is.")
 
 Begin = True
@@ -20,17 +12,16 @@ while Begin:
         HoorntjeBakje = (input("Wilt u deze " + str(bolletjes) + " in een hoorntje of een bakje? :"))
         if HoorntjeBakje in hoorntjeLijst or HoorntjeBakje in bakjeLijst:
             Begin = False
-        else:
-            Sorry()
             Meer = input("Hier is uw " + str(HoorntjeBakje) + " bolletje met " + str(bolletjes) + " bolletjes. Wilt u nog meer bestellen? Y/N :")
             if Meer in Yes:
                 Begin = True
             elif Meer in No:
                 Begin = False
                 print("Bedankt en tot ziens!")
-            else:
-                Sorry()
+            else:   
+                print("Sorry dat sanp ik niet...")
     elif bolletjes >= 4 and bolletjes <=8:
-        print("test 4-8")
+        print("Dan krijgt u van mij een bakje met " + str(bolletjes) + " bolletjes.")
+        Begin = False
     else:
-        Sorry()
+        print("Sorry zulke grote bakken hebben wij niet")
