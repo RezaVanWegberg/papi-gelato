ae906d9de21e88cd8f12fa5694f8ac76a110eb72
Reza <99068159@mydavinci.nl>
2021-11-10 11:34:33 +0100

    bonnetje

--- Papi-Gelato.py
+++ Papi-Gelato.py
@@ -9,12 +9,15 @@ Munt = ["Munt", "munt", "M", "m"]
 Vanille = ["Vanille", "vanille", "V", "v"]
 
 VraagSmaak = True
-# VraagZmaak = 0
+Hoorntje = 0
+Bakje = 0
+AantalBolletjes = 0
 
 Begin = True
 while Begin:
     print("Welkom bij Papi Gelato.")
     bolletjes = int(input("Hoeveel bolletjes wilt u? :"))
+    AantalBolletjes += bolletjes
     if bolletjes <= 3:        
         for smaak in range(bolletjes):
             VraagSmaak = True
@@ -29,6 +32,10 @@ while Begin:
         HoorntjeBakje = (input("Wilt u deze " + str(bolletjes) + " in een hoorntje of een bakje? :"))
         if HoorntjeBakje in hoorntjeLijst or HoorntjeBakje in bakjeLijst:
             Begin = False
+            if HoorntjeBakje in hoorntjeLijst:
+                Hoorntje += 1
+            elif HoorntjeBakje in bakjeLijst:
+                Bakje += 1
             Meer = input("Hier is uw " + str(HoorntjeBakje) + " met " + str(bolletjes) + " bolletjes. Wilt u nog meer bestellen? Y/N :")
             if Meer in Yes:
                 Begin = True
@@ -38,6 +45,8 @@ while Begin:
             else:   
                 print("Sorry dat snap ik niet...")
     elif bolletjes >= 4 and bolletjes <=8:
+        Bakje += 1
+        HoorntjeBakje = bakjeLijst
         print("Dan krijgt u van mij een bakje met " + str(bolletjes) + " bolletjes.")
         Begin = False
     elif bolletjes >8:        
@@ -45,3 +54,18 @@ while Begin:
         Begin = True
     else:
         print("Sorry dat snap ik niet...")
+
+Bolletjes = AantalBolletjes*1.10
+Hoorntjes = Hoorntje*1.25
+Bakjes = Bakje*0.75
+Totaal = Bolletjes + Hoorntjes + Bakjes
+
+print("----------[Papi Gelato]----------")
+print(f"Bolletjes    {bolletjes} X €1.10  = €{Bolletjes:.2f}")
+if HoorntjeBakje in hoorntjeLijst:
+    print(f"Hoorntje     {Hoorntje} X €1,25  = €{Hoorntjes:.2f}")
+else:
+    print(f"Bakje         {Bakje} X €0.75 = €{Bakjes:.2f}")
+
+print("                         -------- +")
+print(f"                        = €{Totaal}")
